6308fe9bcda54b0b21cb9dbac4cacdc233fb2c86
Eric Fabroa <eric.fabroa@ycdsbk12.ca>
2020-12-15 07:50:14 -0500

    Create Dog.java

---
+++ src/oop/lesson5_polymorphism/Dog.java
@@ -0,0 +1,16 @@
+package oop.lesson5_polymorphism;
+
+import oop.lesson5_polymorphism.*;
+
+public class Dog extends Animal{
+    
+    public Dog(){
+        System.out.println("An Dog has been born");
+    }
+    
+    /*
+    public void makeNoise(){
+        System.out.println("ruff!! ruff!!");
+    }
+    */
+}
