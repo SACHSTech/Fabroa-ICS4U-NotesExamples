5d908f799de525af9593982531373cf47c67ba8e
Eric Fabroa <eric.fabroa@ycdsbk12.ca>
2020-12-15 07:52:58 -0500

    Create Cow.java

---
+++ src/oop/lesson5_polymorphism/Cow.java
@@ -0,0 +1,14 @@
+package oop.lesson5_polymorphism;
+
+import oop.lesson5_polymorphism.*;
+
+public class Cow extends Animal{
+    
+    public Cow(){
+        System.out.println("An Cow has been born");
+    }
+    
+    public void makeNoise(){
+        System.out.println("moo!! moo!!");
+    }
+}
