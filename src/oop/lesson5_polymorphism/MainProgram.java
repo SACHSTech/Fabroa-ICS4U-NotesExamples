42b10ddc8edf4eb8d4c4421b1087ce8b573a1336
Eric Fabroa <eric.fabroa@ycdsbk12.ca>
2020-12-15 07:52:10 -0500

    Create MainProgram.java

---
+++ src/oop/lesson5_polymorphism/MainProgram.java
@@ -0,0 +1,20 @@
+package oop.lesson5_polymorphism;
+
+import oop.lesson5_polymorphism.*;
+
+public class MyProgram
+{
+    public static void main(String[] args)
+    {
+        Dog sparky = new Dog();
+        sparky.makeNoise();
+        
+        /*
+        Cow betty = new Cow();
+        betty.makeNoise();
+        
+        Fox larry = new Fox();
+        larry.makeNoise();
+        */
+    }   
+}
